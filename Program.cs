ab3ea07f5a056350934ef693bb3662103e569aa1
NasysMeiner <107574485+NasysMeiner@users.noreply.github.com>
2022-06-19 20:29:00 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -13,11 +13,11 @@ namespace hw3._8
             int[] array = { 1, 2, 3, 4, 5, 6, 7, 8 };
             int a = 0;
 
-            for (int i = array.Length - 1; i > 0; i--)
+            for (int i = 0; i < array.Length - 1; i++)
             {
                 a = array[i];
-                array[i] = array[i - 1];
-                array[i - 1] = a;
+                array[i] = array[i + 1];
+                array[i + 1] = a;
             }
 
             for (int i = 0; i < array.Length; i++)
