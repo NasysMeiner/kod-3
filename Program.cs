0e311eba0a769cae32d0382e4d1b50f32de7c9c7
NasysMeiner <107574485+NasysMeiner@users.noreply.github.com>
2022-07-04 22:46:53 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,190 +4,58 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace hw6._11
+namespace hw7._6
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.CursorVisible = false;
-            Aquarium aquarium = new Aquarium();
-            aquarium.Work();
+            Database database = new Database();
+            database.Work();
         }
     }
 
-    class Aquarium
+    class Database
     {
-        private static int _numberMonths = 0;       
-        private Stack<int> _deathFishs = new Stack<int>();
-        private List<Fish> _fish = new List<Fish>();
+        private List<Soldier> _soldiers = new List<Soldier>();
 
-        public Aquarium()
+        public Database()
         {
-            _fish.Add(new Fish("Oleg", 100));
-            _fish.Add(new Fish("Fish", 3));
+            _soldiers.Add(new Soldier("Игорь", "Автомат", "Рядовой", 10));
+            _soldiers.Add(new Soldier("Андрей", "Пистолет", "Сержант", 15));
+            _soldiers.Add(new Soldier("Ярослав", "Нож", "Прапорщик", 6));
+            _soldiers.Add(new Soldier("Владислав", "Автомат", "Майор", 20));
+            _soldiers.Add(new Soldier("Эдуард", "Пистолет", "Капитан", 30));
+            _soldiers.Add(new Soldier("Илья", "Автомат", "Полковник", 13));
+            _soldiers.Add(new Soldier("Кирилл", "Нож", "Лейтинант", 17));
         }
 
         public void Work()
         {
-            while (true)
+            var soldiers = from Soldier soldier in _soldiers select new { Name = soldier.Name, Rank = soldier.Rank };
+
+            foreach(var soldier in soldiers)
             {
-                Console.SetCursorPosition(80, 0);
-                Console.WriteLine("Аквариум");
-                Console.SetCursorPosition(80, 1);
-                Console.WriteLine("1 - добавить рыбку.");
-                Console.SetCursorPosition(80, 2);
-                Console.WriteLine("2 - убрать рыбку.");
-                Console.SetCursorPosition(0, 0);  
-
-                ShowInfoFish();
-                RemoveFish();
-
-                ConsoleKeyInfo key = Console.ReadKey(true);
-                ChooseItem(key);
-
-                Console.Clear();
-                _numberMonths++;
-            }
-        }
-
-        public void ShowInfoFish()
-        {
-            string condition;
-            int number = 0;            
-
-            Console.WriteLine($"Рыб в аквариуме:{_fish.Count}.\nМесяц:{_numberMonths}\n");
-
-            foreach (Fish fish in _fish)
-            {
-                if(fish.Life)
-                {
-                    condition = "Жива";
-                }
-                else
-                {
-                    condition = "Погибла";
-                }
-
-                Console.WriteLine($"Рыбка: {fish.Name} - {condition}, заявленное время жизни - {fish.LifeTime} месяцев. Id - {fish.FishId}.");
-
-                if(fish.Life == false)
-                {
-                    _deathFishs.Push(number);                
-                }
-
-                fish.RunsTime();
-                number++;
-            }
-        }
-
-        private void RemoveFish()
-        {
-            if(_deathFishs.Count > 0)
-            {
-                int variable = _deathFishs.Count();
-
-                for (int i = 0; i < variable; i++)
-                {
-                    _fish.RemoveAt(_deathFishs.Pop());
-                }
-            }        
-        }
-
-        private void AddFish()
-        {
-            Console.Clear();
-
-            Random random = new Random();
-            string name;           
-
-            Console.Write("Введите имя:");
-            name = Console.ReadLine();
-
-            _fish.Add(new Fish(name, random.Next(2, 10)));
-        }
-
-        private void DeleteFish()
-        {
-            string userInputId;
-            int deleteFish = 0;
-
-            Console.Clear();
-
-            Console.Write("Введите Id рыбки:");
-            userInputId = Console.ReadLine();
-
-            if(int.TryParse(userInputId, out int number))
-            {
-                foreach(Fish fish in _fish)
-                {
-                    if(fish.FishId == number)
-                    {
-                        _fish.Remove(fish);
-                        deleteFish++;
-                        break;
-                    }
-                }
-
-                if(deleteFish > 0)
-                {
-                    Console.WriteLine("Рыбка удалена.");
-                }
-                else
-                {
-                    Console.WriteLine("Рыбка не найдена.");
-                }
-            }
-            else
-            {
-                Console.WriteLine("Рыбки с таким Id нету.");
+                Console.WriteLine($"{soldier.Name} - {soldier.Rank}.");
             }
 
-            Console.ReadKey();
-        }
-
-        private void ChooseItem(ConsoleKeyInfo key)
-        {
-            switch(key.Key)
-            {
-                case ConsoleKey.D1:
-                    AddFish();
-                    break;    
-                case ConsoleKey.D2:
-                    DeleteFish();
-                    break;
-            }
+            Console.ReadLine();
         }
     }
 
-    class Fish
+    class Soldier
     {
-        private static int _globalIds = 10;
-
-        public bool Life { get; private set; }
         public string Name { get; private set; }
-        public int LifeTime { get; private set; }
-        public int FishId { get; private set; }
+        public string Weapon { get; private set; }
+        public string Rank { get; private set; }
+        public int Time { get; private set; }
 
-
-        public Fish(string name, int lifeTime)
+        public Soldier(string name, string weapon, string rank, int time)
         {
             Name = name;
-            LifeTime = lifeTime;
-            Life = true;
-            FishId = _globalIds++;
-        }
-
-        public void RunsTime()
-        {
-            if (LifeTime <= 0)
-            {
-                Life = false;
-            }
-            else
-            {
-                LifeTime--;
-            }            
+            Weapon = weapon;
+            Rank = rank;
+            Time = time;
         }
     }
 }
